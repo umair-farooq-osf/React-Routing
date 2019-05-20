0238fd36866ba176078286b5428add7deff53265
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-20 11:39:04 +0500

    React Router v3 Basics: Code completed for following sections 1. Getting Started with React Router 2. Navigating, Nesting and Redirecting Routes

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { Route, NavLink, Redirect } from 'react-router-dom';
+import HTML from './courses/HTML';
+import CSS from './courses/CSS';
+import JavaScript from './courses/JavaScript';
 
-const Courses = () => (
+const Courses = ({ match }) => (
   <div className="main-content courses">
     <div className="course-header group">
-      <h2>Courses</h2> 
+      <h2>Courses</h2>
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink to={`${match.url}/html`}>HTML</NavLink></li>
+        <li><NavLink to={`${match.url}/css`}>CSS</NavLink></li>
+        <li><NavLink to={`${match.url}/javascript`}>Javascript</NavLink></li>
       </ul>
     </div>
-    
-    {/* Write routes here... */}
+
+    <Route exact path={match.path} render={() => <Redirect to={`${match.path}/html`} />} />
+    <Route path={`${match.path}/html`} component={HTML} />
+    <Route path={`${match.path}/css`} component={CSS} />
+    <Route path={`${match.path}/javascript`} component={JavaScript} />
   </div>
 );
 
