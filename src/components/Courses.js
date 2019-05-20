c199515012594f8622a0aab09623e5f798b87ff5
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-20 11:54:14 +0500

    React Routes 4 Basics: Passing Data to Components Via Routes

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Route, NavLink, Redirect } from 'react-router-dom';
-import HTML from './courses/HTML';
-import CSS from './courses/CSS';
-import JavaScript from './courses/JavaScript';
+import CourseContainer from './courses/CourseContainer';
+import { HTMLCourses, CSSCourses, JSCourses } from '../data/courses';
 
 const Courses = ({ match }) => (
   <div className="main-content courses">
@@ -16,9 +15,9 @@ const Courses = ({ match }) => (
     </div>
 
     <Route exact path={match.path} render={() => <Redirect to={`${match.path}/html`} />} />
-    <Route path={`${match.path}/html`} component={HTML} />
-    <Route path={`${match.path}/css`} component={CSS} />
-    <Route path={`${match.path}/javascript`} component={JavaScript} />
+    <Route path={`${match.path}/html`} render={() => <CourseContainer data={HTMLCourses} />} />
+    <Route path={`${match.path}/css`} render={() => <CourseContainer data={CSSCourses} />} />
+    <Route path={`${match.path}/javascript`} render={() => <CourseContainer data={JSCourses} />} />
   </div>
 );
 
